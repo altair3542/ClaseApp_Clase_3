0bfff1e933b1c983aa356216625f434c1be15303
altair3542 <thiagomesa21@gmail.com>
2025-04-27 10:36:15 -0500

    Implementar validación de nombre y actualizar dependencias

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { useState, useEffect } from 'react'
 
 export default function App() {
-  return (
+  const [nombre, setNombre] = useState('')
+  const [mensaje, setMensaje] = useState('')
+
+  useEffect(() => {
+    if (nombre.length === 0) {
+      setMensaje('');
+    } else if (nombre.length < 3){
+      setMensaje("nombre muy corto")
+    } else {
+      setMensaje("nombre valido")
+    }
+   }, [nombre])
+
+   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.label}>Nombre:</Text>
+
+      <TextInput
+        style={styles.imput}
+        placeholder="Escribe tu nombre"
+        value={nombre}
+        onChangeText={setNombre}
+      />
+
+      {mensaje.length > 0 && (
+        <Text style={[
+          styles.mensaje, mensaje === "nombre valido" ? styles.exito : styles.error
+        ]}>
+          {mensaje}
+        </Text>
+      )}
     </View>
-  );
+   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 24,
+    backgroundColor: "#fff"
   },
-});
+  label: {
+    fontSize: 18,
+    marginBottom: 8
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 6,
+    padding: 12,
+    fontSize: 16,
+    marginBottom: 16
+  },
+  mensaje: {
+    fontSize: 16,
+    margintop: 8
+  },
+  exito: {
+    color: 'green'
+  },
+  error: {
+    color: 'red'
+  }
+})
